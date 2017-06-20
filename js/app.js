fc0c343a1242b25011c761e166654fe292ae94cd
beatorizu <betrizaf@gmail.com>
2017-06-20 15:41:26 -0300

    Add JS commands to calc

---
+++ js/app.js
@@ -0,0 +1,16 @@
+//var resp = confirm("Deseja excluir");
+//
+//console.log(resp)
+
+var n1 = 10;
+var n2 = 13;
+
+function somar(n1, n2) {
+  return n1 + n2;
+}
+
+document.getElementById("btn-calc").onclick = function() {
+  var valorA = parseInt(document.getElementById("valor-a").value);
+  var valorB = parseInt(document.getElementById("valor-b").value);
+  console.log(valorA);
+}
